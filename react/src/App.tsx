1a7160530cb9154419cb431e0bba398894a0c252
alireza-mirshamsi <alireza.mirshamsi96@gmail.com>
2024-02-17 10:21:57 +0330

    update

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,10 +1,14 @@
 import Title from "./components/Title";
+import Content from "./components/content";
 
 function App() {
   return (
     <>
       <Title collegeType="react" />
       <p>Welcome to Quera React + TS College</p>
+      <Content>
+        <p>Welcome to Quera React + TS College</p>
+      </Content>
     </>
   );
 }
---
+++ react/src/components/content.tsx
@@ -0,0 +1,3 @@
+export default function Content({children}: {children: React.ReactNode}){
+    return <div style={{backgroundColor: 'lightcoral'}}>{children}</div>
+}
