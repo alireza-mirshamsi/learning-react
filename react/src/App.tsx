ede0ab50a2086033fa958e82787bd21141e71fde
alireza-mirshamsi <alireza.mirshamsi96@gmail.com>
2024-03-22 14:12:52 +0430

    update

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -2,12 +2,16 @@ import Title from "./components/Title";
 import Content from "./components/content";
 
 function App() {
+  const htmlContent: string = "<h1> hello alireza!, hello </h1>"
   return (
     <>
       <Title collegeType="react" />
       <p>Welcome to Quera React + TS College</p>
       <Content>
         <p>Welcome to Quera React + TS College</p>
+      </Content>
+      <Content>
+        <div dangerouslySetInnerHTML={{__html: htmlContent}}></div>
       </Content>
     </>
   );
